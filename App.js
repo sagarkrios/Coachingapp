3354bdb618d30d34eebcf10d931eec99a93d942e
sagar patil <sagarpatil@Admins-MacBook-Pro.local>
2024-09-28 15:51:56 +0530

    ios setup

--- App.js
+++ App.js
@@ -47,15 +47,14 @@ export const useAppContext = () => React.useContext(AppContext);
 
 
 
-notifee.onForegroundEvent(async ({detail,type}) => {
+notifee.onForegroundEvent(async ({ detail, type }) => {
+  if (type === EventType.PRESS && detail.notification.data.channel_id != "notNotification") {
+    console.log("Notification Clicked in foreground :", detail.notification.data.channel_id);
+    navigate(detail.notification.data.channel_id)
+  }
+  await Promise.resolve();
 
-  if (type === EventType.PRESS  &&detail.notification.data.channel_id!="notNotification") {
-  console.log("Notification Clicked in foreground :", detail.notification.data.channel_id);
-  navigate(detail.notification.data.channel_id)
-    }
-      await Promise.resolve();
-    
- });
+});
 const toastConfig = {
   /*
     Overwrite 'success' type,
@@ -122,7 +121,7 @@ const App = () => {
         if (remoteMessage?.data?.channel_type) {
           console.log("chattting message foreground");
           const message = await chatClient.getMessage(remoteMessage.data.id);
-          onDisplayNotification(message.message.user.name, message.message.text,remoteMessage.data.channel_id);
+          onDisplayNotification(message.message.user.name, message.message.text, remoteMessage.data.channel_id);
           // Get the current badge count
           const currentBadgeCount = await notifee.getBadgeCount();
           // Increment the badge count
@@ -138,6 +137,19 @@ const App = () => {
           onDisplayNotification(data.title, data.body)
         }
       });
+
+    //  This method is triggered when a user clicks on a notification while the app is in the background (not killed but not in the foreground).
+      messaging().onNotificationOpenedApp(async remoteMessage => {
+        if (remoteMessage?.data?.channel_id) {
+          console.log("Notification Clicked in background :", remoteMessage.data.channel_id);
+          navigate(remoteMessage?.data?.channel_id)
+        }
+      });
+      
+       // this method is used to handle notification clicks when the app was completely killed (not running in memory) and was launched as a result of the user clicking a notification.
+      messaging().getInitialNotification(async remoteMessage => {
+        console.log("kill  state", remoteMessage)
+      })
 
       return unsubscribe;
     } catch (error) {
@@ -160,11 +172,11 @@ const App = () => {
       }
     };
     const subscription = AppState.addEventListener('change', handleAppStateChange);
-     // Clean up the subscription
-     return () => {
+    // Clean up the subscription
+    return () => {
       subscription.remove();
     };
-  },[])
+  }, [])
 
 
   return (
@@ -190,7 +202,7 @@ const App = () => {
 
 
 
-export async function onDisplayNotification(title, message,channel_id="notNotification") {
+export async function onDisplayNotification(title, message, channel_id = "notNotification") {
   // Create a channel (required for Android)
   const channelId = await notifee.createChannel({
     id: 'notifeeChaneel',
@@ -207,7 +219,7 @@ export async function onDisplayNotification(title, message,channel_id="notNotifi
     data: {
       channel_id: channel_id, // Add additional data inside the 'data' object
     },
-      android: {
+    android: {
       channelId,
       smallIcon: 'ic_launcher', // Update with your app's small icon
       importance: AndroidImportance.HIGH,
